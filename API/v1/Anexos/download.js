1d270d166699a714f7dba2ebda261c038fb497a3
rasolive <rasolive@hotmail.com>
2022-02-14 20:19:13 -0300

    download gcp documents

--- API/v1/Anexos/download.js
+++ API/v1/Anexos/download.js
@@ -3,19 +3,34 @@ const { Storage } = require('@google-cloud/storage');
 
 exports.download = async (req, res, next) => {
 
-  
+    if (!req.query.path) throw { status: 400, message: 'Bad request' }
+
+    let pathCloud = req.query.path
+    
+    while(pathCloud.includes('%20')) {
+        pathCloud = pathCloud.replace('%20',' ')
+    }
+    while(pathCloud.includes('%C3%B5')) {
+        pathCloud = pathCloud.replace('%C3%B5','õ')
+    }
+    
+    pathCloud = pathCloud.includes('/') ? pathCloud.split('/') : [pathCloud]
+    let fullName = pathCloud.pop()
+    pathCloud.push(fullName)
+    pathCloud = pathCloud.join('/')    
+
     try {        
         const bucketName = process.env.GCP_BUCKET_NAME;
         const storage = new Storage({
             projectId: process.env.GCP_PROJECT_ID || '',
             credentials: JSON.parse(process.env.GCP_CONFIG_PATH), 
         });        
-        // const nameFileOfDB = fullName.includes('_') ? fullName.slice(fullName.indexOf('_') + 1) : fullName
+        const nameFileOfDB = fullName.includes('_') ? fullName.slice(fullName.indexOf('_') + 1) : fullName
         
-        const stream = await storage.bucket(bucketName).file("final/teste/anexos/extras/2022-02-08T08-52-57_Certificado_Nacional_de_Covid-19.pdf").createReadStream();
+        const stream = await storage.bucket(bucketName).file(pathCloud).createReadStream();
 
         res.setHeader("content-type", "application/octet-stream");
-        res.setHeader('Content-disposition', `attachment; filename= ${nameFileOfDB }`)
+        res.setHeader('Content-disposition', `attachment; filename=${nameFileOfDB}`)
         stream.on('error', (err) => {            
             next({ status: 400, message: 'Bad request' })            
         })
