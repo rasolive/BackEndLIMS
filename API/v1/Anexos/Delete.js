c25a868b0d6d78af9aad561a538e8f0879b198d7
rasolive <rasolive@hotmail.com>
2022-02-14 21:17:19 -0300

    move files to quarentena

--- API/v1/Anexos/Delete.js
+++ API/v1/Anexos/Delete.js
@@ -1,3 +1,5 @@
+
+// Move to quarentena
 require('dotenv').config()
 const { Storage } = require('@google-cloud/storage');
 
@@ -13,7 +15,7 @@ exports.deleteFile = async (req, res, next) => {
             credentials: JSON.parse(process.env.GCP_CONFIG_PATH), 
         });        
             
-        const response = await storage.bucket(bucketName).file(fileName).delete();
+        const response = await storage.bucket(bucketName).file(fileName).move(`quarentena/${fileName}`);
 
         console.log(`gs://${bucketName}/${fileName} deleted`);
 
@@ -24,3 +26,35 @@ exports.deleteFile = async (req, res, next) => {
         next({ status: 400, message: 'Bad request' })
 }
 }
+
+
+
+//                DELETE
+
+
+// require('dotenv').config()
+// const { Storage } = require('@google-cloud/storage');
+
+// exports.deleteFile = async (req, res, next) => {
+
+//     const fileName= req.body.fileName
+//     console.log(fileName)
+  
+//     try{         
+//         const bucketName = process.env.GCP_BUCKET_NAME;
+//         const storage = new Storage({
+//             projectId: process.env.GCP_PROJECT_ID || '',
+//             credentials: JSON.parse(process.env.GCP_CONFIG_PATH), 
+//         });        
+            
+//         const response = await storage.bucket(bucketName).file(fileName).delete();
+
+//         console.log(`gs://${bucketName}/${fileName} deleted`);
+
+//         return res.send(`${fileName} deleted`)
+        
+//     } catch (err) {
+       
+//         next({ status: 400, message: 'Bad request' })
+// }
+// }
