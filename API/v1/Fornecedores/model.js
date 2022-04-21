02ffc0ecd6299b0367f9cf58f57bbcce98d03357
rasolive <rasolive@hotmail.com>
2022-04-21 20:08:04 -0300

    fix modedl fornecedores

--- API/v1/Fornecedores/model.js
+++ API/v1/Fornecedores/model.js
@@ -5,12 +5,20 @@ const { Schema } = mongoose;
 
 // const TableSchema = new Schema({ _id: Number, evidencia: String, fonte: String, detentor: String });
 
-const Fornecedores = new Schema({
+const Suppliers = new Schema({
 	_id: Number,
 	active: Boolean,
-	nome: String,
-	endereco: {type: Array, default: {} },
-	telefone: {type: Array, default: {} },
+	cod: Number,
+	name: String,
+	rua: String,
+	numero: String,
+	bairro: String,
+	cidade: String,
+	estado: String,
+	cep: String,
+	telefone: String,
+	email: String,
+	//telefone: {type: Array, default: {} },
 	cnpj: String,
 	createdBy: String,
 	updatedBy: String,
@@ -18,4 +26,4 @@ const Fornecedores = new Schema({
 	timestamps: true,
 });
 
-module.exports = mongoose.model('Fornecedores', Fornecedores, 'fornecedores');
+module.exports = mongoose.model('Suppliers', Suppliers, 'suppliers');
