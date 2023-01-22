2ab01274c348cc5d90b7adde6eee216b788c1734
rasolive <rasolive@hotmail.com>
2023-01-21 23:36:28 -0300

    configure e-mail template

--- API/v1/Email/controller.js
+++ API/v1/Email/controller.js
@@ -52,47 +52,56 @@ exports.forgotPassword = async (req, res, next) => {
     body.validPass = false
     body.user = 'System'
 
-    mensagem = `nova senha: '${newPass}'`
+    mensagem = `<p>Olá,<br></br>
+    &emsp; Você solicitou o reset de senha do sistema applims.net. Faça seu login no serviço
+    com a nova senha informada abaixo e, em seguida, troque sua senha.
+    <br></br>
+    Senha: <strong>${newPass}</strong>
+    <br></br>
+    Em caso de problemas <a href="https://applims.herokuapp.com/contato">entre em contato com o administrador do sistema.</a>
+    <br></br>
+    E-mail enviado automáticamente pelo sistema, favor não responder.
+    <br></br>
+    Obrigado,
+    <br></br>
+    <a href="https://applims.herokuapp.com">applims.net</a>
+</p>`
 
 
     const usuario = await Users.findOne({ email })
-
-    console.log(usuario)
-    console.log('newPass',newPass)
-    console.log('body',body)
 
     if (usuario) {
 
         const response = await update(usuario._id, body, Users);
 
-        console.log('response', response)
+        if (response._id === usuario._id) {
 
-        if(response._id === usuario._id){
-            return res.status(200).send({ response: `senha alterada` })
+            const mailOptions = {
+                from: process.env.GMAIL,
+                to: req.body.email,
+                subject: 'Redefinição de senha applims.net',
+                html: mensagem
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                    return res.json(INTERNALSERVERERROR).end()
+
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    return res.json(SUCCESS).end()
+                    // do something useful
+                }
+            });
         }
     }
-    else{
+    else {
 
-    return res.status(402).send({ response: `usuário ${email} não encontrado` })
-}
+        return res.status(400).send({ status: 400, response: `usuário ${email} não encontrado` })
+    }
 
 };
 
-// const mailOptions = {
-//     from: process.env.GMAIL,
-//     to: req.body.email,
-//     subject: 'Redefinição de senha applims.net',
-//     text: mensagem
-// };
 
-// transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//         console.log(error);
-//         return res.json(INTERNALSERVERERROR).end()
 
-//     } else {
-//         console.log('Email sent: ' + info.response);
-//         return res.json(SUCCESS).end()
-//         // do something useful
-//     }
-// });
