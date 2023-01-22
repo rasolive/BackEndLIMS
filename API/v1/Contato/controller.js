ee5db8fc2806284a147d54d87d6c1a5b2ff856c3
rasolive <rasolive@hotmail.com>
2023-01-22 00:02:41 -0300

    send e-mail from form

--- API/v1/Contato/controller.js
+++ API/v1/Contato/controller.js
@@ -1,29 +1,62 @@
 const { Contato } = require('../../../Models');
+const nodemailer = require('nodemailer');
 const { create, findById, findList, remove } = require('../../../repositories');
 const { INTERNALSERVERERROR, BADREQUEST } = require('../../../Globals/httpErros');
 const { errorLog } = require('../../../Globals/utils');
+const { text } = require('body-parser');
+
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.GMAIL,
+		pass: process.env.GMAIL_PASS
+	}
+});
 
 // POST
 exports.post = async (req, res, next) => {
 
-		const user = "Formulário de contato"
-	
-		const result = await create(req.body, user, Contato);
+	const mailOptions = {
+		from: process.env.GMAIL,
+		to: 'rasolive@hotmail.com',
+		subject: 'Formulário de contato applims.net',
+		html: `<p>${req.body.msg}<br></br>
+			enviado por: ${req.body.name}
+			<br></br>
+			E-mail: ${req.body.email}
+		</p>`
+	};
 
-		if (result.status >= 400) {
-			throw errorLog("Não foi possivel criar", INTERNALSERVERERROR);
-		}
+	const user = "Formulário de contato"
 
-		if (result && result.message._id) {
-			return res.json(result).end();
-		} else {
-			throw errorLog("Não foi possivel criar", INTERNALSERVERERROR);
-		}
+	const result = await create(req.body, user, Contato);
+
+	if (result.status >= 400) {
+		throw errorLog("Não foi possivel criar", INTERNALSERVERERROR);
+	}
+
+	if (result && result.message._id) {
+
+		transporter.sendMail(mailOptions, function (error, info) {
+			if (error) {
+				console.log(error);
+				throw errorLog("Não foi possivel criar", INTERNALSERVERERROR);
+
+			} else {
+				console.log('Email sent: ' + info.response);
+				return res.json(result).end();
+				// do something useful
+			}
+
+		});
+	} else {
+
+	}
 }
 
 // GET list
 exports.getList = async (req, res, next) => {
-    try {
+	try {
 		//const { tokenUser } = res.session;
 
 		const returnList = await findList({}, Contato);
@@ -40,21 +73,21 @@ exports.getList = async (req, res, next) => {
 // GET by ID
 exports.getById = async (req, res, next) => {
 	try {
-		
-        const validate = [ req.params.id ];
+
+		const validate = [req.params.id];
 
 		if (!validate.every(item => Boolean(item) === true)) {
 			throw BADREQUEST;
 		}
-        
-        const options = {}
+
+		const options = {}
 		Object.assign(options, { active: true })
 		Object.assign(options, req.query || {})
-		delete options.token;        
+		delete options.token;
 
 		const returnList = await findById(req.params.id, options, Contato);
-		
-        if (!returnList) return res.json({}).end();
+
+		if (!returnList) return res.json({}).end();
 
 		return res.json(returnList).end();
 
@@ -70,15 +103,15 @@ exports.getById = async (req, res, next) => {
 exports.deleteById = async (req, res, next) => {
 	try {
 
-		const validate = [ req.params.id ];
+		const validate = [req.params.id];
 
 		if (!validate.every(item => Boolean(item) === true)) {
 			throw BADREQUEST;
 		}
-		
+
 		const body = req.body
 
-    	body.user = req.user.email
+		body.user = req.user.email
 
 		const returnList = await remove(req.params.id, body, Contato);
 
@@ -89,7 +122,7 @@ exports.deleteById = async (req, res, next) => {
 		}
 	} catch (error) {
 		return (
-			next(errorLog("delete.catch "+error, (error && error.status) ? error : INTERNALSERVERERROR))
+			next(errorLog("delete.catch " + error, (error && error.status) ? error : INTERNALSERVERERROR))
 		)
 	}
 }
